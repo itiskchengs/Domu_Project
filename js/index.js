b98831d01a27acc0206481d297fa3e13ff3e8bc0
itiskchengs <itskcheng@gmail.com>
2020-07-07 18:11:55 -0400

    This is the first verison of the DOMU project

---
+++ js/index.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $('.plates_section, .sweets_section, .drinks_section').hide();
+
+    $('#menuButton1').click(function(){
+        $('.plates_section').hide();
+        $('.sweets_section').hide();
+        $('.drinks_section').hide();
+        $('.ramen_section').show();
+    });
+    $('#menuButton2').click(function(){
+        $('.ramen_section').hide();
+        $('.sweets_section').hide();
+        $('.drinks_section').hide();
+        $('.plates_section').show();
+
+    });
+    $('#menuButton3').click(function(){
+        $('.ramen_section').hide();
+        $('.plates_section').hide();
+        $('.drinks_section').hide();
+        $('.sweets_section').show();
+    });
+    $('#menuButton4').click(function() {
+        $('.ramen_section').hide();
+        $('.plates_section').hide();
+        $('.sweets_section').hide();
+        $('.drinks_section').show();
+    });
+});
