5118993ae6be3b29e550c3051ad4e79a684d62f2
itiskchengs <itskcheng@gmail.com>
2020-08-23 16:35:14 -0400

    final changes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,33 @@
+/*NAVIGATION*/
+const menuBtn = document.querySelector('.menu-btn');
+const hamburger = document.querySelector('.menu-btn_burger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.menu-nav_item');
 
+let showMenu = false;
 
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu(){
+    if(!showMenu) {
+        hamburger.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open');
+        navItems.forEach(item => item.classList.add('open'));
+
+    showMenu = true;
+    }else{
+        hamburger.classList.remove('open');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
+        navItems.forEach(item => item.classList.remove('open'));
+
+    showMenu = false;
+    }
+}
+
+/*MENU SORT*/
 $(document).ready(function() {
     $('.plates_section, .sweets_section, .drinks_section').hide();
 
@@ -29,3 +57,79 @@ $(document).ready(function() {
         $('.drinks_section').show();
     });
 });
+
+/*ANIME*/
+
+let mainHeader = document.querySelector('.main-header');
+mainHeader.innerHTML = mainHeader.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.main-header .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i
+  }).add({
+    targets: '.main-header .letter',
+    duration: 1100,
+    delay: (el, i) => 100 + 30 * i
+  });
+
+  /*SCROLL MAGIC*/
+
+  $(document).ready(function() {
+        let controller = new ScrollMagic.Controller();
+
+        //build a scene
+
+        let ourScene = new ScrollMagic.Scene({
+            triggerElement: '.joinUsContainer',
+            offset: -600
+        })
+        .setClassToggle('.joinUsContainer', 'slide-up')
+        .addTo(controller);
+  });
+
+  $(document).ready(function() {
+    let controller = new ScrollMagic.Controller();
+
+    //build a scene
+
+    let ourScene = new ScrollMagic.Scene({
+        triggerElement: '.domuSelectionLeft',
+        offset: -400
+    })
+    .setClassToggle('.domuSelectionLeft', 'slide-left')
+    .addTo(controller);
+});
+
+$(document).ready(function() {
+    let controller = new ScrollMagic.Controller();
+
+    //build a scene
+
+    let ourScene = new ScrollMagic.Scene({
+        triggerElement: '.domuSelectionRight',
+        offset: -400
+    })
+    .setClassToggle('.domuSelectionRight', 'slide-right')
+    .addTo(controller);
+});
+
+$(document).ready(function() {
+    let controller = new ScrollMagic.Controller();
+
+    //build a scene
+
+    let ourScene = new ScrollMagic.Scene({
+        triggerElement: '.aboutContainer',
+        offset: -400
+    })
+    .setClassToggle('.aboutContainer', 'fade-in')
+    .addTo(controller);
+});
+
